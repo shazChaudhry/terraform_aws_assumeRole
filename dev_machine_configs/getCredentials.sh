f69e7046c6869bb978877734666f6658ee233ef5
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2020-06-20 17:20:58 +0100

    Made config more genric for public use

--- dev_machine_configs/getCredentials.sh
+++ dev_machine_configs/getCredentials.sh
@@ -2,7 +2,7 @@
 
 aws_credentials=$(aws sts assume-role \
 --role-arn arn:aws:iam::<AWS_ACCOUNT_2_ID>:role/<ROLE_NAME> \
---role-session-name devRole \
+--role-session-name <USER_NAME> \
 --profile dev)
 export AWS_ACCESS_KEY_ID=$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
 export AWS_SECRET_ACCESS_KEY=$(echo $aws_credentials|jq '.Credentials.SecretAccessKey'|tr -d '"')
